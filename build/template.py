cd764f70b71781535d121a5adfdbce6a351dbe35
Dustin Schau <dustinschau@gmail.com>
2016-09-21 01:07:01 -0500

    style: tweak style to look nicer

--- build/template.py
+++ build/template.py
@@ -10,7 +10,7 @@ for template in dir(custom_templates):
   if template.startswith('filter_'):
     env.filters[template.split('filter_')[-1]] = getattr(custom_templates, template)
 
-template = env.get_template('index.template')
+template = env.get_template('html-dschau.template')
 
 rendered = template.render(load_json(open('mock/template.json').read())).strip()
 
